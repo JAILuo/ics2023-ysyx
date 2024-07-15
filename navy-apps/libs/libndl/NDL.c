933473b1f5b2c9de94101f1407d214c5ecb41d35
JAILuo <1670756477@qq.com>
2024-07-15 22:12:51 +0800

    pa3.3: part of ioe keyboard

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -1,19 +1,30 @@
+#include <assert.h>
+#include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
+uint32_t start_time = 0; // enough? 2038?
+
+// return the system time in milliseconds
 uint32_t NDL_GetTicks() {
-  return 0;
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000 + tv.tv_usec / 1000) - start_time;
 }
 
+// 读出一条事件信息, 将其写入`buf`中, 最长写入`len`字节
+// 若读出了有效的事件, 函数返回1, 否则返回0
 int NDL_PollEvent(char *buf, int len) {
-  return 0;
+    assert(evtdev != -1);
+    return read(evtdev, buf, len);
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
@@ -57,6 +68,12 @@ int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
+
+  evtdev = open("/dev/events", "r");
+
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  start_time = (uint32_t)(tv.tv_sec * 1000 + tv.tv_usec / 1000);
   return 0;
 }
 
--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-#include <sys/time.h>
+#include <NDL.h>
 
 int main() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
+    NDL_Init(0);
 
+    uint32_t time = NDL_GetTicks();
     size_t ms = 500;
     while (1) {
-        while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
-            gettimeofday(&tv, NULL);
+        while (time < ms) {
+            time = NDL_GetTicks();
         }
-        printf("tv.tv_sec: %ld ", tv.tv_sec);
-        printf("ms = %lu\n", ms);
+        printf("second: %lu\n", ms / 1000);
+        printf("ms: %lu\n", ms);
         ms += 500;
     }
 }
