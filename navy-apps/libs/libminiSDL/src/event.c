3a9ca9001f4aef07274722dfa9391b7d198d4ab6
JAILuo <1670756477@qq.com>
2024-07-19 00:57:59 +0800

    pa3.3 1. nterm run

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -15,7 +15,24 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+    char buf[64];
+    char key_type[8], key_name[8];
+    memset(buf, '\0', sizeof(buf));
+
+    // RTFM
+    if (NDL_PollEvent(buf, sizeof(buf)) == 0)
+        return 0;
+
+    sscanf(buf, "%s %s\n", key_type, key_name);
+    ev->key.type = buf[1] == 'd' ? SDL_KEYDOWN : SDL_KEYUP;
+
+    for(size_t i = 0; i < NR_KEYS; ++i){
+        if (strcmp(key_name, keyname[i]) == 0) {
+            ev->key.keysym.sym = i;
+            break;
+        }
+    }
+    return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
@@ -35,7 +52,6 @@ int SDL_WaitEvent(SDL_Event *event) {
         }
     }
     return 1;
-
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
