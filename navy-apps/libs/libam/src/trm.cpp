e146ba8c72ec2a9112d553f33e64ea2b8d44bf36
JAILuo <1670756477@qq.com>
2024-07-19 21:04:34 +0800

    pa3.3 1. add __ISA__ macro for different platform 2. fceux run (notice:__NO_FILE_SYSTEM__ in fceux-am/src/drivers/sdl/sdl.cpp)

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -2,15 +2,28 @@
 
 Area heap;
 
-#define nemu_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
+#define __riscv__
+
+#if defined(__X86__)
+# define nemu_trap(code) asm volatile ("int3" : :"a"(code))
+#elif defined(__MIPS32__)                                                                                             
+# define nemu_trap(code) asm volatile ("move $v0, %0; sdbbp" : :"r"(code))
+#elif defined(__riscv__)
+# define nemu_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
+#elif defined(__LOONGARCH32R__)
+# define nemu_trap(code) asm volatile("move $a0, %0; break 0" : :"r"(code))
+#elif
+# error unsupported ISA __ISA__
+#endif
+
 
 void putch(char ch) {
     putchar(ch);
 }
 
 void halt(int code) {
-      nemu_trap(code);
-
-  // should not reach here
-  while (1);
+    nemu_trap(code);
+    
+    // should not reach here
+    while (1);
 }
